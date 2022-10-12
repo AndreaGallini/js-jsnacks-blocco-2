d103b8c03030f9ba179125758a582cb76563f7b3
AndreaGallini <andrea.gallini01@libero.it>
2022-10-12 16:58:45 +0200

    esecizio 3 fine

--- js/script3.js
+++ js/script3.js
@@ -1,5 +1,6 @@
 let numeri = [0,1,2,3,4,5,6,7,8,9,10];
 let somma = document.getElementById('risultato').value;
+let risultato = document.getElementById('risultato');
 let totale = 0;
 console.log(numeri)
 for(let i = 1; i< numeri.length; i++){
@@ -7,9 +8,9 @@ for(let i = 1; i< numeri.length; i++){
   if(numeri[i] % 2 != 0 ){
     let numeriDispari = numeri[i]
     console.log(numeriDispari)
-   
-  
+    totale += numeriDispari
+  console.log(totale)
+  risultato.innerHTML = totale
 }
- totale += numeri[i]
- console.log(totale)
+
 }
