a8e754fd1a978444f41fe0d38c356ebe1ea3816f
AndreaGallini <andrea.gallini01@libero.it>
2022-10-12 15:36:44 +0200

    iniizo 3

---
+++ js/script3.js
@@ -0,0 +1,11 @@
+let numeri = [0,1,2,3,4,5,6,7,8,9,10];
+let somma = document.getElementById('risultato').value;
+console.log(numeri)
+for(let i = 1; i< numeri.length; i++){
+  // console.log(i)
+  if(numeri[i] % 2 != 0 ){
+    let numeriDispari = numeri[i]
+    console.log(numeriDispari)
+    
+}
+}
